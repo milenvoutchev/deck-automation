e4c236e9fb2acb1212ae7003abf28eef1de25f98
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-10-18 00:38:01 +0200

    add babel symlink in bin

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# add babel to .bin folder (not added by heroku, maybe as npm install hooks are not run or whatever)
+(cd node_modules/.bin && ln -s ../babel-cli/bin/babel.js babel)
 yarn prod:build
+
+# link to "static" folders from build (missing, as babel creates only .js files)
 (cd build && ln -s ../server/public public)
 (cd build && ln -s ../server/views views)
