d9f02beee697901b44b48f29786ab65b5df83e7f
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-10-22 21:49:06 +0200

    homepage layout

--- server/l10n/index.js
+++ server/l10n/index.js
@@ -1,6 +1,6 @@
 // no l10n yet (unless you are English-speaker, then you're all set ;))
 module.exports = {
   home: {
-    title: 'Welcome to Deck Automation',
+    title: 'Deck Automation',
   },
 };
